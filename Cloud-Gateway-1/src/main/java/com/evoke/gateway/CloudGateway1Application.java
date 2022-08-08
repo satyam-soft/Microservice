f85acbc5f10e15bef5b7746a69f2295ca56efc21
ssingh3 <ssingh3@SSINGH3-L-2802.evoketechnologies.com>
2022-08-08 23:52:09 +0530

    first commit

---
+++ Cloud-Gateway-1/src/main/java/com/evoke/gateway/CloudGateway1Application.java
@@ -0,0 +1,19 @@
+package com.evoke.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableHystrix
+public class CloudGateway1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudGateway1Application.class, args);
+	}
+
+}
---
+++ Cloud-Gateway-1/src/test/java/com/evoke/gateway/CloudGateway1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evoke.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudGateway1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
